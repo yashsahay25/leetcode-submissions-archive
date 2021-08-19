29bbff48d759fefd850bc5f11927baf7a2b65ab3
yash.sahay <yashsahay2022@gmail.com>
2021-08-19 12:23:07 +0530

    1669 merge-in-between-linked-lists

---
+++ 1669_merge-in-between-linked-lists_540917014.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        ListNode* t1=list1;
+        ListNode* t2=list1;
+        
+        ListNode* last=list2;
+        while(last->next)
+            last =last->next;
+        
+        for(int i=0;i<a-1;i++)
+            t1=t1->next;
+        for(int i=0;i<=b;i++)
+            t2=t2->next;
+        
+        t1->next= list2;
+        last->next=t2;
+        
+        return list1;
+    }
+};
+
+       /* ListNode *f=list1,*s=list1;
+        for(int i=0;i<a-1;i++) f=f->next;
+        
+        for(int i=0;i<b;i++) s=s->next;
+        
+        f->next=list2;
+        while(list2->next!=NULL)
+            list2=list2->next;
+        
+        list2->next=s->next;
+        return list1;*/
