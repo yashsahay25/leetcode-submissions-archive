efab62de31ed182ce7551d18bb770d45248df6c9
yash.sahay <yashsahay2022@gmail.com>
2022-01-20 20:55:22 +0530

    1 two-sum

---
+++ 1_two-sum_624184951.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            //2nd number to find
+            int k=target-nums[i];
+            //if found, then add to res & return res
+            if(mp.find(k)!=mp.end())
+            {
+                res.push_back(mp[k]);
+                res.push_back(i);
+                return res;
+            }
+            //add nums[i] to map
+            mp[nums[i]]=i;
+        }
+        return res;
+    }
+};
