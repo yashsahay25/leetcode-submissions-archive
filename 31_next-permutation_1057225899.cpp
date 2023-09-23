98f0b0510888d113748d7a093e5e5a88accccbe9
yash.sahay <yashsahay2022@gmail.com>
2023-09-23 17:00:12 +0530

    31 next-permutation

---
+++ 31_next-permutation_1057225899.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int idx = -1;
+        for(int i=n-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                idx = i;
+                break;
+            }
+        }
+        if(idx==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        } 
+        for(int i=n-1;i>=idx;i--){
+            if(nums[i]>nums[idx-1]){
+                swap(nums[i],nums[idx-1]);
+                break;
+            }
+        }
+        reverse(nums.begin()+idx,nums.end());
+        return;
+    }
+};
