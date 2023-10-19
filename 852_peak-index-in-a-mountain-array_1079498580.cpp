1244f424317e24b170252127da8d6c53c9101b19
yash.sahay <yashsahay2022@gmail.com>
2023-10-19 21:25:26 +0530

    852 peak-index-in-a-mountain-array

---
+++ 852_peak-index-in-a-mountain-array_1079498580.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size(); //Size of array.
+
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            //If arr[mid] is the peak:
+            if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
+                return mid;
+
+            // If we are in the left:
+            if (arr[mid] > arr[mid - 1]) low = mid + 1;
+
+            // If we are in the right:
+            // Or, arr[mid] is a common point:
+            else high = mid - 1;
+        }
+        // Dummy return statement
+        return -1;
+    }
+};
