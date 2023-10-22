21c3518a9a4c47812028a7845c7dc36c5eebe42f
yash.sahay <yashsahay2022@gmail.com>
2023-10-22 18:52:52 +0530

    33 search-in-rotated-sorted-array

---
+++ 33_search-in-rotated-sorted-array_1081546208.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lo = 0;  //start
+        int hi = nums.size()-1;  //end
+        
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+            if(nums[mid] == target){    //target found
+                return mid;
+            }
+            else if(nums[lo] <= nums[mid]){  //left half is sorted
+                if(nums[lo] <= target and target <= nums[mid]){  
+                    //target b/w start & mid
+                    hi = mid-1;  //search in that left half
+                }
+                else{
+                    lo = mid+1;  //search in the right half
+                }
+            }
+            else{   //right is sorted
+                if(nums[mid] <= target and target <= nums[hi]){
+                    // target b/w mid & end  
+                    lo = mid+1; // search in right half
+                }
+                else{
+                    hi = mid-1; // search in left half
+                }
+            }  
+        }
+        return -1;
+    }
+};
