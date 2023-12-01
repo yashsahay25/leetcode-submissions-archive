b2992b03bb135ad3b2c67972a7b540300db78697
yash.sahay <yashsahay2022@gmail.com>
2023-12-01 19:57:09 +0530

    47 permutations-ii

---
+++ 47_permutations-ii_1110432385.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void backtrack(vector<vector<int>>& result, vector<int>& nums,int index){
+        if(index >= nums.size()){
+            result.push_back(nums);
+            return;
+        }
+        unordered_set<int> s;
+        for(int i=index;i<nums.size();i++){
+            if(s.find(nums[i]) != s.end()) continue; // skip duplicates at i
+            s.insert(nums[i]);  // mark as used in set
+            swap(nums[i],nums[index]);
+            backtrack(result,nums,index+1);
+            swap(nums[i],nums[index]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
+        // instead of passing current array, we will directly use nums array
+        backtrack(result,nums,0); // call swap based recursion
+        return result;
+    }
+};
