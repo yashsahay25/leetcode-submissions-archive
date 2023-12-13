1f4f1459b438724c443c93ae38d3a22103603614
yash.sahay <yashsahay2022@gmail.com>
2023-12-14 05:07:18 +0530

    36 valid-sudoku

---
+++ 36_valid-sudoku_1119334768.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> s;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                char current = board[i][j];
+                if(current!='.'){
+                    string row = "row"+to_string(i)+current;
+                    string col = "col"+to_string(j)+current;
+                    string box = "box"+to_string(i/3)+to_string(j/3)+current;
+                    
+                    if(s.count(row))    return false;
+                    s.insert(row);
+
+                    if(s.count(col))    return false;
+                    s.insert(col);
+
+                    if(s.count(box))    return false;
+                    s.insert(box);
+                }
+            }
+        }
+        return true;
+    }
+};
