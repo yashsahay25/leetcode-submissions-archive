4e4799ee8689e758663a23d4906cffdd3ac1d3c8
yash.sahay <yashsahay2022@gmail.com>
2023-12-29 21:54:37 +0530

    1310 xor-queries-of-a-subarray

---
+++ 1310_xor-queries-of-a-subarray_1131727542.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        int q = queries.size();
+        vector<int> res(q); // result array
+        
+        // precompute prefix xor array
+        int n = arr.size();
+        vector<int> preXor(n);
+        preXor[0]=arr[0];
+        for(int i=1;i<n;i++){
+            preXor[i]=arr[i]^preXor[i-1];
+        }
+
+        for(int i=0;i<q;i++){
+            int left = queries[i][0];
+            int right = queries[i][1];
+            res[i] = preXor[left]^preXor[right]^arr[left];
+        }
+        return res;
+    }
+};
