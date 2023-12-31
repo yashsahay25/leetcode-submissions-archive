4491fd6324fb9d3df11fd056bbe1bfb300239dfb
yash.sahay <yashsahay2022@gmail.com>
2023-12-31 08:27:59 +0530

    476 number-complement

---
+++ 476_number-complement_1132891055.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int bits = (int)log2(num)+1;
+        long nearestPowerOf2 = pow(2,bits)-1;
+        if(nearestPowerOf2 < num) 
+            return (int)nearestPowerOf2;
+        return (int)(nearestPowerOf2 - num);
+    }
+};
