8e2689530d9f00314878ffd82a53bdf8e295fb0e
yash.sahay <yashsahay2022@gmail.com>
2024-01-18 13:54:05 +0530

    496 next-greater-element-i

---
+++ 496_next-greater-element-i_1149795706.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+        vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+        {
+            unordered_map<int, int> mp;
+            stack<int> s;
+            for (int i = nums2.size() - 1; i >= 0; i--)
+            {
+                while (!s.empty() && s.top() <= nums2[i])
+                    s.pop();
+                if (s.empty())
+                    mp[nums2[i]] = -1;
+                else
+                    mp[nums2[i]] = s.top();
+                s.push(nums2[i]);
+            }
+            vector<int> res;
+            for (int x: nums1)
+            {
+                res.push_back(mp[x]);
+            }
+            return res;
+        }
+};
