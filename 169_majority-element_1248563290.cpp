261e903f69189a270709fe020af210ec279041cf
yash.sahay <yashsahay2022@gmail.com>
2024-05-03 20:36:27 +0530

    169 majority-element

---
+++ 169_majority-element_1248563290.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int majority = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                majority = num;
+            }
+            
+            if (num == majority) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return majority;
+    }
+};
