9b64c02259d6fab5cad5d485dfe7ffd5692edce7
yash.sahay <yashsahay2022@gmail.com>
2024-05-12 14:43:53 +0530

    128 longest-consecutive-sequence

---
+++ 128_longest-consecutive-sequence_1256179647.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        // Hashing BFS
+        if(nums.size()==0) return 0;
+
+        unordered_map<int, bool> visited;
+        for (int i = 0; i < nums.size(); i++) {
+            visited.insert({nums[i], false});
+        }
+
+        int maxLen = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            int current = nums[i], len = 1;
+            if (visited[current] == true)
+                continue;
+
+            visited[current] = true;
+            int next = current + 1, prev = current - 1;
+            while (visited.find(next) != visited.end()) {
+                len++;
+                visited[next] = true;
+                next++;
+            }
+            while (visited.find(prev) != visited.end()) {
+                len++;
+                visited[prev] = true;
+                prev--;
+            }
+            maxLen = max(maxLen, len);
+        }
+        return maxLen;
+    }
+};
