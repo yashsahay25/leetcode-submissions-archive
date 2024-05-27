5defbac6af4219f68d08f4bdeaec6bba42b53841
yash.sahay <yashsahay2022@gmail.com>
2024-05-27 20:10:44 +0530

    135 candy

---
+++ 135_candy_1269784491.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+
+        // left side
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // right side
+        for (int i = n - 1; i >= 1; i--) {
+            if (ratings[i - 1] > ratings[i]) {
+                candies[i - 1] = max(candies[i] + 1, candies[i - 1]);
+            }
+        }
+
+        // reuse left array to store max(L,R)
+        int sum = 0;
+        for (int c : candies)
+            sum += c;
+        return sum;
+    }
+};
