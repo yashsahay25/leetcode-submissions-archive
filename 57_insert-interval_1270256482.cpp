793660b7b7dc7d9d33bf90d076e381eaae1011eb
yash.sahay <yashsahay2022@gmail.com>
2024-05-28 09:15:27 +0530

    57 insert-interval

---
+++ 57_insert-interval_1270256482.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> mergedIntervals;
+        
+        sort(intervals.begin(),intervals.end());
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+
+        for(int i=1;i<intervals.size();i++){
+            // compare current interval end with existing ka start
+            // merge them, & update end
+            if(intervals[i][0]<=end){
+                end=max(end,intervals[i][1]);
+            }
+            else{
+                // new interval found, push existing start,end
+                mergedIntervals.push_back({start,end});
+                start=intervals[i][0];
+                end=intervals[i][1];
+            }
+        }
+        mergedIntervals.push_back({start,end}); // handle last element
+        return mergedIntervals;
+    }
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        // add newInterval to list
+        intervals.push_back(newInterval);
+
+        // sort list
+        sort(intervals.begin(),intervals.end());
+
+        // call mergeIntervals on updated list
+        return merge(intervals);
+    }
+};
