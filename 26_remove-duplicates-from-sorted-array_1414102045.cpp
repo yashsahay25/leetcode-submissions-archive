09544e0942e9bc012063f0b72cd3bf3245313149
yash.sahay <yashsahay2022@gmail.com>
2024-10-06 20:44:32 +0530

    26 remove-duplicates-from-sorted-array

---
+++ 26_remove-duplicates-from-sorted-array_1414102045.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=1,j=1;
+        while(j<nums.size()){
+            if(nums[j]==nums[j-1]) j++;
+            else{
+                nums[i]=nums[j];
+                i++;
+                j++;
+            }
+        }
+        return i;
+    }
+};
