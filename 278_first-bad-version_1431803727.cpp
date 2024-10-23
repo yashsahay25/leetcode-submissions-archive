ab4a9adf4c581d063e29c734fa415851d8df766a
yash.sahay <yashsahay2022@gmail.com>
2024-10-23 20:01:18 +0530

    278 first-bad-version

---
+++ 278_first-bad-version_1431803727.cpp
@@ -0,0 +1,16 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        // find the first bad version
+        int l=1, r=n, m;
+        while(l<r){
+            m = l+(r-l)/2;
+            if(!isBadVersion(m)) l=m+1;
+            else r=m;
+        }
+        return l;
+    }
+};
