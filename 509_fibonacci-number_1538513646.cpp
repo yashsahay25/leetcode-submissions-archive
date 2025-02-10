0ce73ab0d6fd4334a9dfed76a921711ab98704d4
yash.sahay <yashsahay2022@gmail.com>
2025-02-10 19:48:27 +0530

    509 fibonacci-number

---
+++ 509_fibonacci-number_1538513646.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        int prev = 1, prev2 = 0;
+        for (int i = 2; i <= n; i++) {
+            int curr = prev2 + prev;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+};
