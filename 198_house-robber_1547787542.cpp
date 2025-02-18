e33be0e5a0aa3fe404d2507a181d939e16774f94
yash.sahay <yashsahay2022@gmail.com>
2025-02-18 19:29:38 +0530

    198 house-robber

---
+++ 198_house-robber_1547787542.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return nums[0];
+
+        int prev, prev2;
+        prev2 = nums[0];
+        prev = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int curr = max(nums[i] + prev2, prev);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+};
