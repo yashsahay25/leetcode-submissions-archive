f5dc08eb9788c05f02680c1430704e400477c1ba
yash.sahay <yashsahay2022@gmail.com>
2025-02-18 19:26:09 +0530

    198 house-robber

---
+++ 198_house-robber_1547784734.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int pick = nums[i] + dp[i - 2];
+            int notPick = dp[i - 1];
+            dp[i] = max(pick, notPick);
+        }
+        return dp[n-1];
+    }
+};
