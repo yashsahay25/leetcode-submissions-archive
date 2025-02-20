86789aff07fc2385eefd5bbf1f324b54888c3aa8
yash.sahay <yashsahay2022@gmail.com>
2025-02-20 18:09:10 +0530

    63 unique-paths-ii

---
+++ 63_unique-paths-ii_1549942190.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        // Base case
+        if (grid[0][0] == 1) return 0;
+        dp[0][0] = 1;
+
+        // Fill DP table
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    dp[i][j] = 0; // Obstacle
+                } else {
+                    if (i > 0) dp[i][j] += dp[i - 1][j];
+                    if (j > 0) dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
