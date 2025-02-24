a1c9e81d036b98779a93eedfbe6fa50e7324bb2f
yash.sahay <yashsahay2022@gmail.com>
2025-02-24 16:45:32 +0530

    120 triangle

---
+++ 120_triangle_1554026893.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        // start from 2nd last row & go up
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle[i].size(); j++) {
+                triangle[i][j] = triangle[i][j] + min(triangle[i + 1][j],
+                                                      triangle[i + 1][j + 1]);
+            }
+        }
+        return triangle[0][0];
+    }
+};
