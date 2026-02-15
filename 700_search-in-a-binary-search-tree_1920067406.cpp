f24e6d41b03474ad620e7abe7ee47d7dc2762f53
yash.sahay <yashsahay2022@gmail.com>
2026-02-15 12:57:59 +0530

    700 search-in-a-binary-search-tree

---
+++ 700_search-in-a-binary-search-tree_1920067406.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int key) {
+        while (root && root->val != key) {
+            root = (root->val > key) ? root->left : root->right;
+        }
+        return root;
+    }
+};
